77bb408eb5ddb03ba6e83e742944ddf0d2229eae
Bruno Borri <bruno.borri@bureauveritas.com>
2014-01-15 18:16:52 +0100

    UI : PictureOverlay progression

--- src/yuni/ui/pictureoverlay.cpp
+++ src/yuni/ui/pictureoverlay.cpp
@@ -17,39 +17,81 @@ namespace UI
 		::glBindTexture(GL_TEXTURE_2D, pTexture->id());
 		shader->bindUniform("Texture0", Yuni::Gfx3D::Vertex<>::vaTexture0);
 
-		int offsetX = 0;
-		int offsetY = 0;
+		const float texWidth = (float)pTexture->width();
+		const float texHeight = (float)pTexture->height();
+		const float overlayWidth = (float)pWidth;
+		const float overlayHeight = (float)pHeight;
+		// pDisplay == podNone
+		float offsetX = 0.0f;
+		float offsetY = 0.0f;
 		if (pDisplay == podOffset)
 		{
-			offsetX = pOffsetX;
-			offsetY = pOffsetY;
+			offsetX = (float)pOffsetX;
+			offsetY = (float)pOffsetY;
 		}
-		float minX = 0.0f;
-		float maxX = 1.0f;
-		float minY = 0.0f;
-		float maxY = 1.0f;
+		else if (pDisplay == podCenter)
+		{
+			offsetX = (texWidth - overlayWidth) / 2.0f;
+			offsetY = (texHeight - overlayHeight) / 2.0f;
+		}
+		// Check if the whole overlay is out of bounds of the texture
+		if (offsetX > texWidth || offsetY > texHeight ||
+			offsetX + overlayWidth < 0.0f || offsetY + overlayHeight < 0.0f)
+			return;
+		float minTexX = 0.0f;
+		float maxTexX = 1.0f;
+		float minTexY = 0.0f;
+		float maxTexY = 1.0f;
+		float xStart = pX;
+		float yStart = pY;
+		float xEnd = pX + overlayWidth;
+		float yEnd = pY + overlayHeight;
 		switch (pDisplay)
 		{
 			case podStretch:
 				// Nothing to do, coordinates are perfect
 				break;
 			case podNone:
-				if (pTexture->width() > pWidth)
-					maxX = (float)pWidth / (float)pTexture->width();
-				else if (pTexture->width() < pWidth)
-				{
-					// TODO : Fix vertices
-				}
-				if (pTexture->height() > pHeight)
-					maxY = (float)pHeight / (float)pTexture->height();
-				else if (pTexture->height() < pHeight)
-				{
-					// TODO : Fix vertices
-				}
-				break;
 			case podOffset:
 			case podCenter:
+				if (offsetX > 0.0f)
+				{
+					// Fix texture coordinates
+					minTexX = offsetX / texWidth;
+					if (minTexX < 0.0f)
+						minTexX = 0.0f;
+				}
+				if (texWidth > overlayWidth + offsetX)
+				{
+					// Fix texture coordinates
+					maxTexX = pWidth + offsetX / texWidth;
+					if (maxTexX > 1.0f)
+						maxTexX = 1.0f;
+				}
+				else
+				{
+					// Fix vertices
+				}
+				if (offsetY > 0.0f)
+				{
+					minTexY = offsetY / texHeight;
+					if (minTexY < 0.0f)
+						minTexY = 0.0f;
+				}
+				if (texHeight > overlayHeight + offsetY)
+				{
+					maxTexY = pHeight + offsetY / texHeight;
+					if (maxTexY > 1.0f)
+						maxTexY = 1.0f;
+				}
+				else
+				{
+					// Fix vertices
+				}
+				break;
 			case podFit:
+				// TODO
+				break;
 			case podFill:
 				// TODO
 				break;
@@ -61,25 +103,25 @@ namespace UI
 		// Set texture coordinates
 		const float texCoord[] =
 			{
-				minX, minY,
-				minX, maxY,
-				maxX, minY,
-				maxX, minY,
-				minX, maxY,
-				maxX, maxY
+				minTexX, minTexY,
+				minTexX, maxTexY,
+				maxTexX, minTexY,
+				maxTexX, minTexY,
+				minTexX, maxTexY,
+				maxTexX, maxTexY
 			};
 		::glEnableVertexAttribArray(Gfx3D::Vertex<>::vaTextureCoord);
 		::glVertexAttribPointer(Gfx3D::Vertex<>::vaTextureCoord, 2, GL_FLOAT, 0, 0, texCoord);
 		// Set vertex positions
 		const float vertices[] =
 			{
-				(float)pX, (float)pY,
-				(float)pX, (float)(pY + height()),
-				(float)(pX + width()), (float)pY,
-				(float)(pX + width()), (float)pY,
-				(float)pX, (float)(pY + height()),
-				(float)(pX + width()), (float)(pY + height())
-				};
+				xStart, yStart,
+				xStart, yEnd,
+				xEnd, yStart,
+				xEnd, yStart,
+				xStart, yEnd,
+				xEnd, yEnd
+			};
 		::glEnableVertexAttribArray(Gfx3D::Vertex<>::vaPosition);
 		::glVertexAttribPointer(Gfx3D::Vertex<>::vaPosition, 2, GL_FLOAT, 0, 0, vertices);
 		// Draw
