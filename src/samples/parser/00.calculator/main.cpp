fd334feb2066d9e2ea51824157db4d1fcc9e0d28
Damien GERARD <damien@iwi.me>
2014-07-18 01:20:49 +0200

    sample: updated/fixed parser/00.calculator

--- src/samples/parser/00.calculator/main.cpp
+++ src/samples/parser/00.calculator/main.cpp
@@ -1,10 +1,27 @@
 
-#include "calculator-grammar.h"
+#include <yuni/yuni.h>
+#include <yuni/core/system/console.h>
+#include "calculator.h"
+#include <iostream>
+
 
 
 int main()
 {
 	Demo::Calculator::Parser parser;
-	parser.load("4+3*(9.42-2)");
-	return 0;
+
+	if (parser.load("4+3*(9.42-2)"))
+	{
+		// Colors only when the attached console is a TTY
+		bool withColors = Yuni::System::Console::IsStdoutTTY();
+
+		Yuni::Clob output;
+		Demo::Calculator::Node::Export(output, *parser.root, withColors);
+		std::cout << output << std::endl;
+		return EXIT_SUCCESS;
+	}
+
+
+	std::cerr << "failed to parse\n";
+	return EXIT_FAILURE;
 }
