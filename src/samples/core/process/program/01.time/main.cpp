042c77c1cf81351e89dd8eb1c970edde6aee6a49
Damien GERARD <damien@iwi.me>
2014-12-16 23:44:52 +0100

    sample: Yuni::Process::Program: time: broadcast signals SIGINT (SIGTERM as a bonus) on UNIX to properly handle ctrl-c

--- src/samples/core/process/program/01.time/main.cpp
+++ src/samples/core/process/program/01.time/main.cpp
@@ -3,8 +3,31 @@
 #include <yuni/datetime/timestamp.h>
 #include <yuni/core/getopt.h>
 #include <iostream>
+#ifdef YUNI_OS_UNIX
+#include <signal.h>
+#endif
 
 using namespace Yuni;
+
+
+static Process::Program process;
+
+
+#ifdef YUNI_OS_UNIX
+void signalHandler(int sig)
+{
+	// sending the same signal to the sub-process. If the sub-process
+	// handles the signal, it may have a chance to stop properly and should behave
+	// the same than without being executed by this program.
+	process.signal(sig);
+
+	if (sig == SIGINT) // ctrl-c
+	{
+		// a new line to keep the output clean (^C)
+		std::cout << std::endl;
+	}
+}
+#endif
 
 
 
@@ -18,7 +41,6 @@ int main(int argc, char* argv[])
 		return EXIT_FAILURE;
 	}
 
-	Process::Program process;
 
 	// providing arguments
 	process.program(argv[1]);
@@ -30,6 +52,14 @@ int main(int argc, char* argv[])
 	// redirect the output to the console
 	process.redirectToConsole(true);
 
+	#ifdef YUNI_OS_UNIX
+	// install signal handler for SIGINT (ctrl-c) and SIGTERM (quit)
+	// see note in the 'signalHandler' routine
+	::signal(SIGINT,  signalHandler); // interrupt program (^C ctrl-c)
+	::signal(SIGTERM, signalHandler); // software termination signal
+	#endif
+
+
 	// execute the command
 	sint64 duration = 0;
 	int exitStatus = process.execute() ? process.wait(&duration) : EXIT_FAILURE;
@@ -40,7 +70,10 @@ int main(int argc, char* argv[])
 	for (int i = 2; i < argc; ++i)
 		std::cout << ' ' << argv[i];
 
-	std::cout << "  " << duration << "ms\n";
+	std::cout << "  " << duration << "ms";
+	if (exitStatus != 0)
+		std::cout << " (exit: " << exitStatus << ')';
+	std::cout << '\n';
 	return exitStatus;
 }
 
