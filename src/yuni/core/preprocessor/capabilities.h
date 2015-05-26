f0ad0ad7bc83a5c1d561a51b82c799a55ffe3faf
Damien GERARD <damien@iwi.me>
2015-05-26 23:38:01 +0200

    capabilities: code simplification

--- src/yuni/core/preprocessor/capabilities.h
+++ src/yuni/core/preprocessor/capabilities.h
@@ -1,6 +1,4 @@
-#ifndef __YUNI_PREPROCESSOR_OS_DETECTION_H__
-# define __YUNI_PREPROCESSOR_OS_DETECTION_H__
-
+#pragma once
 /* !!! "C compatibility" header !!! */
 
 
@@ -94,43 +92,43 @@
 ** All those informations can be found at http://predef.sourceforge.net/
 */
 
-# include <assert.h>
+#include <assert.h>
 
-# define YUNI_OS_FLAG_WINDOWS  0
-# define YUNI_OS_FLAG_UNIX     0
-# define YUNI_OS_FLAG_LINUX    0
-# define YUNI_OS_FLAG_MACOS    0
+#define YUNI_OS_FLAG_WINDOWS  0
+#define YUNI_OS_FLAG_UNIX     0
+#define YUNI_OS_FLAG_LINUX    0
+#define YUNI_OS_FLAG_MACOS    0
 
 
-# if defined(__TOS_WIN__) || defined(__WIN32__) || defined(_WIN64) || defined(_WIN32) || defined(YUNI_OS_WINDOWS)
+#if defined(__TOS_WIN__) || defined(__WIN32__) || defined(_WIN64) || defined(_WIN32) || defined(YUNI_OS_WINDOWS)
 #	include "windows.h"
-# else
+#else
 #	include "unixes.h"
-# endif
+#endif
 
 
 
 /* Window System */
-# ifdef YUNI_OS_WINDOWS
+#ifdef YUNI_OS_WINDOWS
 #	define YUNI_WINDOWSYSTEM_MSW
-# else
+#else
 #	ifdef YUNI_OS_MAC
 #		define YUNI_WINDOWSYSTEM_COCOA
 #	else
 #		define YUNI_WINDOWSYSTEM_X11
 #	endif
-# endif
+#endif
 
 /* LLVM */
-# ifdef __llvm__
+#ifdef __llvm__
 #	define YUNI_OS_LLVM
-# endif
+#endif
 
-# ifdef __clang__
+#ifdef __clang__
 #	define YUNI_OS_CLANG
 #	define YUNI_COMPILER_NAME "Compatible Clang Compiler"
 #	define YUNI_OS_CLANG_VERSION (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__) // compatible mode
-# else
+#else
 /* GNU C and C++ compiler */
 #	ifdef __GNUC__
 #		define YUNI_OS_GCC
@@ -141,46 +139,46 @@
 #	else
 #		define YUNI_OS_GCC_VERSION 0
 #	endif
-# endif
+#endif
 
 
 /* Intel Compiler */
-# if defined(__INTEL_COMPILER) || defined(__ICC)
+#if defined(__INTEL_COMPILER) || defined(__ICC)
 #	define YUNI_OS_INTELCOMPILER
 #	ifdef YUNI_COMPILER_NAME /* in some cases, the compiler may already have been detected as Visual Studio */
 #		undef YUNI_COMPILER_NAME
 #	endif
 #	define YUNI_COMPILER_NAME "ICC (Intel C++ Compiler)"
-# endif
+#endif
 
 /* Como Compiler */
-# ifdef __COMO__
+#ifdef __COMO__
 #	define YUNI_OS_COMO
 #	ifdef YUNI_COMPILER_NAME
 #		undef YUNI_COMPILER_NAME
 #	endif
 #	define YUNI_COMPILER_NAME "Comeau C++"
-# endif
+#endif
 
 /* CrayC Compiler */
-# ifdef __CRAYC__
+#ifdef __CRAYC__
 #	define YUNI_OS_CRAYC
 #	ifdef YUNI_COMPILER_NAME
 #		undef YUNI_COMPILER_NAME
 #	endif
 #	define YUNI_COMPILER_NAME "Cray C/C++"
-# endif
+#endif
 
 
 
 /* 32/64 Bits modes */
-# if !defined(YUNI_OS_32) && !defined(YUNI_OS_64)
+#if !defined(YUNI_OS_32) && !defined(YUNI_OS_64)
 #	if defined(__IA64__) || defined(_IA64) || defined(__amd64__) || defined(__x86_64__) || defined(_M_IA64) || defined(_WIN64) || defined(__MINGW64__)
 #		 define YUNI_OS_64
 #	else
 #		 define YUNI_OS_32
 #	endif
-# endif
+#endif
 
 
 
@@ -198,15 +196,15 @@
 ** }
 ** \endcode
 */
-# if !defined(YUNI_ALWAYS_INLINE) && defined(YUNI_HAS_GCC_ALWAYS_INLINE)
+#if !defined(YUNI_ALWAYS_INLINE) && defined(YUNI_HAS_GCC_ALWAYS_INLINE)
 #	define YUNI_ALWAYS_INLINE  __attribute__((always_inline))
-# endif
-# if !defined(YUNI_ALWAYS_INLINE) && defined(YUNI_HAS_MSVC_FORCE_INLINE)
+#endif
+#if !defined(YUNI_ALWAYS_INLINE) && defined(YUNI_HAS_MSVC_FORCE_INLINE)
 #	define YUNI_ALWAYS_INLINE  __forceinline
-# endif
-# if !defined(YUNI_ALWAYS_INLINE)
+#endif
+#if !defined(YUNI_ALWAYS_INLINE)
 #	define YUNI_ALWAYS_INLINE  inline
-# endif
+#endif
 
 
 /*!
@@ -225,12 +223,12 @@
 ** }
 ** \endcode
 */
-# if !defined(YUNI_ATTR_PURE) && defined(YUNI_HAS_GCC_ATTR_PURE)
+#if !defined(YUNI_ATTR_PURE) && defined(YUNI_HAS_GCC_ATTR_PURE)
 #	define YUNI_ATTR_PURE  __attribute__((pure))
-# endif
-# if !defined(YUNI_ATTR_PURE)
+#endif
+#if !defined(YUNI_ATTR_PURE)
 #	define YUNI_ATTR_PURE
-# endif
+#endif
 
 
 /*!
@@ -250,12 +248,12 @@
 ** }
 ** \endcode
 */
-# if !defined(YUNI_ATTR_CONST) && defined(YUNI_HAS_GCC_ATTR_CONST)
+#if !defined(YUNI_ATTR_CONST) && defined(YUNI_HAS_GCC_ATTR_CONST)
 #	define YUNI_ATTR_CONST  __attribute__((const))
-# endif
-# if !defined(YUNI_ATTR_CONST)
+#endif
+#if !defined(YUNI_ATTR_CONST)
 #	define YUNI_ATTR_CONST
-# endif
+#endif
 
 
 
@@ -265,13 +263,13 @@
 
 
 
-# ifndef YUNI_HAS_CPP_KEYWORD_OVERRIDE
+#ifndef YUNI_HAS_CPP_KEYWORD_OVERRIDE
 #	define override
-# endif
+#endif
 
-# ifndef YUNI_HAS_CPP_KEYWORD_FINAL
+#ifndef YUNI_HAS_CPP_KEYWORD_FINAL
 #	define final
-# endif
+#endif
 
 
 
@@ -280,21 +278,21 @@
 ** \def YUNI_DEPRECATED
 ** \brief Deprecated
 */
-# if defined(YUNI_OS_GCC)
+#if defined(YUNI_OS_GCC)
 #	if (__GNUC__ > 3 || (__GNUC__ == 3 && __GNUC_MINOR__ >= 1))
 #		define YUNI_DEPRECATED(text, func...)  func __attribute__((__deprecated__))
 #	endif
-# endif
-# if defined(_MSC_VER) && (_MSC_VER >= 1300)
+#endif
+#if defined(_MSC_VER) && (_MSC_VER >= 1300)
 #	if (_MSC_FULL_VER >= 140050320)
 #		define YUNI_DEPRECATED(text, func, ...) __declspec(deprecated(text)) func
 #	else
 #		define YUNI_DEPRECATED(text, func, ...) __declspec(deprecated) func
 #	endif
-# endif
-# ifndef YUNI_DEPRECATED
+#endif
+#ifndef YUNI_DEPRECATED
 #	define YUNI_DEPRECATED(text, func...) func
-# endif
+#endif
 
 /*!
 ** \def Noreturn
@@ -305,38 +303,38 @@
 ** }
 ** \endcode
 */
-# if defined(YUNI_OS_GCC)
+#if defined(YUNI_OS_GCC)
 #	if (__GNUC__ > 3 || (__GNUC__ == 3 && __GNUC_MINOR__ >= 1))
 #		define YUNI_NORETURN  __attribute__((noreturn))
 #	endif
-# endif
-# ifndef YUNI_NORETURN
+#endif
+#ifndef YUNI_NORETURN
 #	define YUNI_NORETURN
-# endif
+#endif
 
 
 
 
 /* C++0x */
-# if defined(__GXX_EXPERIMENTAL_CPP0X__) || defined(_RWSTD_EXT_CXX_0X)
+#if defined(__GXX_EXPERIMENTAL_CPP0X__) || defined(_RWSTD_EXT_CXX_0X)
 #	define YUNI_CPP_0X
-# endif
+#endif
 
 
 
 /* OS Detection */
-# ifndef YUNI_OS_NAME
+#ifndef YUNI_OS_NAME
 #	define YUNI_OS_NAME "Unknown"
 #	define YUNI_OS_UNKNOWN
 #	warning "OS Detection: Unable to guess the operating system"
-# endif
+#endif
 
 /* Compiler Detection */
-# ifndef YUNI_COMPILER_NAME
+#ifndef YUNI_COMPILER_NAME
 #	define YUNI_COMPILER_UNKNOWN
 #	define YUNI_COMPILER_NAME "Unknown"
 #	warning "Compiler Detection: Unable to guess the compiler"
-# endif
+#endif
 
 
 
@@ -354,15 +352,15 @@
 ** \see http://gcc.gnu.org/onlinedocs/gcc-4.2.4/gcc/Thread_002dLocal.html
 ** \see http://msdn.microsoft.com/en-us/library/6e298fy4(VS.71).aspx
 */
-# if defined(YUNI_OS_BORLAND)
+#if defined(YUNI_OS_BORLAND)
 #	define YUNI_THREAD_LOCAL_STORAGE(type)  type __thread
-# else
+#else
 #	if defined(YUNI_OS_WINDOWS) && (defined(YUNI_OS_MSVC) || defined(__INTEL_COMPILER))
 #		define YUNI_THREAD_LOCAL_STORAGE(type)  __declspec(thread) type
 #	else
 #		define YUNI_THREAD_LOCAL_STORAGE(type)  __thread type
 #	endif
-# endif
+#endif
 
 
 
@@ -372,9 +370,9 @@
 **
 ** You should use va_end to release the new list
 */
-# ifdef YUNI_HAS_VA_COPY
+#ifdef YUNI_HAS_VA_COPY
 #	define YUNI_VA_COPY(dst, src)  va_copy((dst), (src))
-# else
+#else
 #	if (defined(__GNUC__) && defined(__PPC__) && (defined(_CALL_SYSV) || defined(__WIN32__))) || defined(__WATCOMC__)
 #		define YUNI_VA_COPY(dst, src)	  (*(dst) = *(src))
 #	elif defined(YUNI_VA_COPY_AS_ARRAY)
@@ -382,16 +380,16 @@
 #	else /* va_list is a pointer */
 #		define YUNI_VA_COPY(dst, src)	  ((dst) = (src))
 #	endif
-# endif
+#endif
 
 
 /* Force the define YUNI_DYNAMIC_LIBRARY (Visual Studio) */
-# if !defined(YUNI_DYNAMIC_LIBRARY) && defined(_WINDLL)
+#if !defined(YUNI_DYNAMIC_LIBRARY) && defined(_WINDLL)
 #	define YUNI_DYNAMIC_LIBRARY
 #	define YUNI_DYNAMIC_LIBRARY_EXPORT
-# endif
+#endif
 
-# ifdef YUNI_DYNAMIC_LIBRARY
+#ifdef YUNI_DYNAMIC_LIBRARY
 #	ifdef YUNI_OS_MSVC
 #		ifdef YUNI_DYNAMIC_LIBRARY_EXPORT
 #			define YUNI_DECL             __declspec(dllexport)
@@ -406,43 +404,43 @@
 #			define YUNI_EXPIMP_TEMPLATE  extern
 #		endif
 #	endif
-# endif
+#endif
 /* Fallback to empty */
-# ifndef YUNI_DECL
+#ifndef YUNI_DECL
 #	define YUNI_DECL
-# endif
-# ifndef YUNI_EXPIMP_TEMPLATE
+#endif
+#ifndef YUNI_EXPIMP_TEMPLATE
 #	define YUNI_EXPIMP_TEMPLATE
-# endif
+#endif
 
 
 
 /* In some cases, the macro 'unix' and 'linux' can be defined */
-# ifdef unix
+#ifdef unix
 #	undef unix
-# endif
-# ifdef linux
+#endif
+#ifdef linux
 #	undef linux
-# endif
+#endif
 
 
-# ifndef YUNI_OS_GCC_VERSION
+#ifndef YUNI_OS_GCC_VERSION
 #	define YUNI_OS_GCC_VERSION 0
-# endif
+#endif
 
 
 
-# ifdef YUNI_OS_MSVC
+#ifdef YUNI_OS_MSVC
 // Annoying warning from Visual Studio :
 // decorated name length exceeded, name was truncated (when using a lot of templates)
 #	pragma warning( disable : 4503)
-# endif
+#endif
 
 
 
 
 /* Memcpy */
-# ifndef NDEBUG
+#ifndef NDEBUG
 #	ifdef YUNI_OS_MSVC
 #		define YUNI_MEMCPY(dst, dstsize, source, count)   do \
 			{ \
@@ -456,7 +454,7 @@
 				memcpy(dst, source, (size_t)count); \
 			} while(0)
 #	endif
-# else
+#else
 #	ifdef YUNI_OS_MSVC
 #		define YUNI_MEMCPY(dst, dstsize, source, count) \
 			(void) memcpy_s(dst, (size_t) dstsize, source, (size_t) count)
@@ -464,7 +462,7 @@
 #		define YUNI_MEMCPY(dst, dstsize, source, count) \
 			(void) memcpy(dst, source, (size_t) count)
 #	endif
-# endif
+#endif
 
 
 
@@ -478,23 +476,19 @@
 **     does something else
 ** \endcode
 */
-# if defined(YUNI_OS_GCC) || defined(YUNI_HAS_GCC_BUILTIN_EXPECT)
+#if defined(YUNI_OS_GCC) || defined(YUNI_HAS_GCC_BUILTIN_EXPECT)
 #	define YUNI_LIKELY(x)     __builtin_expect(!!(x), 1)
 #	define YUNI_UNLIKELY(x)   __builtin_expect(!!(x), 0)
-# else
+#else
 #	define YUNI_LIKELY(x)     (x)
 #	define YUNI_UNLIKELY(x)   (x)
-# endif
+#endif
 
 
 
-# ifdef YUNI_OS_GCC
-# else
-# endif
 
 
-
-# ifdef __cplusplus /* Only with a C++ Compiler */
+#ifdef __cplusplus /* Only with a C++ Compiler */
 
 namespace Yuni
 {
@@ -528,8 +522,5 @@ namespace System
 //#	endif
 //# endif
 
-# endif
+#endif /* __cplusplus */
 
-
-
-#endif /* __YUNI_PREPROCESSOR_OS_DETECTION_H__ */
