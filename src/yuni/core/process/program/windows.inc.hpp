c4d583f42ea3fbdd637ad3a08e54b8165feb7967
Bruno Borri <bruno.borri@bureauveritas.com>
2015-06-08 16:18:41 +0200

    Trying to make Yuni::Process work for Windows.

--- src/yuni/core/process/program/windows.inc.hpp
+++ src/yuni/core/process/program/windows.inc.hpp
@@ -7,7 +7,10 @@
 // http://stackoverflow.com/questions/53208/how-do-i-automatically-destroy-child-processes-in-windows
 // http://stackoverflow.com/questions/985281/what-is-the-closest-thing-windows-has-to-fork/985525#985525
 
-
+//# include "../../system/windows.hdr.h"
+# include <windows.h>
+# include <WinNT.h>
+# include <setjmp.h>
 
 namespace Yuni
 {
@@ -17,10 +20,171 @@ namespace Process
 	namespace // anonymous
 	{
 
+#define NtCurrentProcess() ((HANDLE)-1)
+#define NtCurrentThread() ((HANDLE) -2)
 
-		static bool HaveLoadedFunctionsForFork(void)
+		typedef struct _SYSTEM_HANDLE_INFORMATION
 		{
-			HANDLE ntdll = ::GetModuleHandle("ntdll");
+			ULONG ProcessId;
+			UCHAR ObjectTypeNumber;
+			UCHAR Flags;
+			USHORT Handle;
+			PVOID Object;
+			ACCESS_MASK GrantedAccess;
+		} SYSTEM_HANDLE_INFORMATION, *PSYSTEM_HANDLE_INFORMATION;
+
+		typedef struct _OBJECT_ATTRIBUTES
+		{
+			ULONG Length;
+			HANDLE RootDirectory;
+			PVOID /* really PUNICODE_STRING */  ObjectName;
+			ULONG Attributes;
+			PVOID SecurityDescriptor;       /* type SECURITY_DESCRIPTOR */
+			PVOID SecurityQualityOfService; /* type SECURITY_QUALITY_OF_SERVICE */
+		} OBJECT_ATTRIBUTES, *POBJECT_ATTRIBUTES;
+
+		typedef enum _MEMORY_INFORMATION_
+		{
+			MemoryBasicInformation,
+			MemoryWorkingSetList,
+			MemorySectionName,
+			MemoryBasicVlmInformation
+		} MEMORY_INFORMATION_CLASS;
+
+		typedef struct _CLIENT_ID
+		{
+			HANDLE UniqueProcess;
+			HANDLE UniqueThread;
+		} CLIENT_ID, *PCLIENT_ID;
+
+		typedef struct _USER_STACK
+		{
+			PVOID FixedStackBase;
+			PVOID FixedStackLimit;
+			PVOID ExpandableStackBase;
+			PVOID ExpandableStackLimit;
+			PVOID ExpandableStackBottom;
+		} USER_STACK, *PUSER_STACK;
+
+		typedef LONG KPRIORITY;
+		typedef ULONG_PTR KAFFINITY;
+		typedef KAFFINITY *PKAFFINITY;
+
+		typedef struct _THREAD_BASIC_INFORMATION
+		{
+			LONG ExitStatus;
+			PVOID TebBaseAddress;
+			CLIENT_ID ClientId;
+			KAFFINITY AffinityMask;
+			KPRIORITY Priority;
+			KPRIORITY BasePriority;
+		} THREAD_BASIC_INFORMATION, *PTHREAD_BASIC_INFORMATION;
+
+		typedef enum _THREAD_INFORMATION_CLASS
+		{
+			ThreadBasicInformation,
+			ThreadTimes,
+			ThreadPriority,
+			ThreadBasePriority,
+			ThreadAffinityMask,
+			ThreadImpersonationToken,
+			ThreadDescriptorTableEntry,
+			ThreadEnableAlignmentFaultFixup,
+			ThreadEventPair,
+			ThreadQuerySetWin32StartAddress,
+			ThreadZeroTlsCell,
+			ThreadPerformanceCount,
+			ThreadAmILastThread,
+			ThreadIdealProcessor,
+			ThreadPriorityBoost,
+			ThreadSetTlsArrayAddress,
+			ThreadIsIoPending,
+			ThreadHideFromDebugger
+		} THREAD_INFORMATION_CLASS, *PTHREAD_INFORMATION_CLASS;
+
+		typedef enum _SYSTEM_INFORMATION_CLASS { SystemHandleInformation = 0x10 } SYSTEM_INFORMATION_CLASS;
+
+		typedef LONG (NTAPI *ZwWriteVirtualMemory_t)(
+			IN HANDLE ProcessHandle,
+			IN PVOID BaseAddress,
+			IN PVOID Buffer,
+			IN ULONG NumberOfBytesToWrite,
+			OUT PULONG NumberOfBytesWritten OPTIONAL);
+
+		typedef LONG (NTAPI *ZwCreateProcess_t)(
+			OUT PHANDLE ProcessHandle,
+			IN ACCESS_MASK DesiredAccess,
+			IN POBJECT_ATTRIBUTES ObjectAttributes,
+			IN HANDLE InheriteFromProcessHandle,
+			IN BOOLEAN InheritHandles,
+			IN HANDLE SectionHandle OPTIONAL,
+			IN HANDLE DebugPort OPTIONAL,
+			IN HANDLE ExceptionPort OPTIONAL);
+
+		typedef LONG (WINAPI *ZwQuerySystemInformation_t)(
+			SYSTEM_INFORMATION_CLASS SystemInformationClass,
+			PVOID SystemInformation,
+			ULONG SystemInformationLength,
+			PULONG ReturnLength);
+
+		typedef LONG (NTAPI *ZwQueryVirtualMemory_t)(
+			IN HANDLE ProcessHandle,
+			IN PVOID BaseAddress,
+			IN MEMORY_INFORMATION_CLASS MemoryInformationClass,
+			OUT PVOID MemoryInformation,
+			IN ULONG MemoryInformationLength,
+			OUT PULONG ReturnLength OPTIONAL);
+
+		typedef LONG (NTAPI *ZwGetContextThread_t)(
+			IN HANDLE ThreadHandle,
+			OUT PCONTEXT Context);
+
+		typedef LONG (NTAPI *ZwCreateThread_t)(
+			OUT PHANDLE ThreadHandle,
+			IN ACCESS_MASK DesiredAccess,
+			IN POBJECT_ATTRIBUTES ObjectAttributes,
+			IN HANDLE ProcessHandle,
+			OUT PCLIENT_ID ClientId,
+			IN PCONTEXT ThreadContext,
+			IN PUSER_STACK UserStack,
+			IN BOOLEAN CreateSuspended);
+
+		typedef LONG (NTAPI *ZwResumeThread_t)(
+			IN HANDLE ThreadHandle,
+			OUT PULONG SuspendCount OPTIONAL);
+
+		typedef LONG (NTAPI *ZwClose_t)(IN HANDLE ObjectHandle);
+
+		typedef LONG (NTAPI *ZwQueryInformationThread_t)(
+			IN HANDLE ThreadHandle,
+			IN THREAD_INFORMATION_CLASS ThreadInformationClass,
+			OUT PVOID ThreadInformation,
+			IN ULONG ThreadInformationLength,
+			OUT PULONG ReturnLength OPTIONAL);
+
+
+		static ZwCreateProcess_t ZwCreateProcess = nullptr;
+		static ZwQuerySystemInformation_t ZwQuerySystemInformation = nullptr;
+		static ZwQueryVirtualMemory_t ZwQueryVirtualMemory = nullptr;
+		static ZwCreateThread_t ZwCreateThread = nullptr;
+		static ZwGetContextThread_t ZwGetContextThread = nullptr;
+		static ZwResumeThread_t ZwResumeThread = nullptr;
+		static ZwClose_t ZwClose = nullptr;
+		static ZwQueryInformationThread_t ZwQueryInformationThread = nullptr;
+		static ZwWriteVirtualMemory_t ZwWriteVirtualMemory = nullptr;
+
+		static jmp_buf jenv;
+
+		static int ChildEntry()
+		{
+			::longjmp(jenv, 1);
+			return 0;
+		}
+
+
+		static bool HaveLoadedFunctionsForFork()
+		{
+			auto ntdll = ::GetModuleHandle(L"ntdll");
 			if (ntdll == nullptr)
 				return false;
 
@@ -59,7 +223,7 @@ namespace Process
 		}
 
 
-		static int Fork()
+		static intptr_t Fork()
 		{
 			HANDLE hProcess = 0;
 			HANDLE hThread = 0;
@@ -86,25 +250,25 @@ namespace Process
 				return -1;
 
 			// create forked process
-			::ZwCreateProcess(&hProcess, PROCESS_ALL_ACCESS, &oa,
-				::NtCurrentProcess(), true, 0, 0, 0);
+			ZwCreateProcess(&hProcess, PROCESS_ALL_ACCESS, &oa,
+				NtCurrentProcess(), true, 0, 0, 0);
 
 			// set the Eip for the child process to our child function
-			::ZwGetContextThread(::NtCurrentThread(), &context);
+			ZwGetContextThread(NtCurrentThread(), &context);
 
 			// In x64 the Eip and Esp are not present,
 			// their x64 counterparts are Rip and Rsp respectively.
 #if _WIN64
-			context.Rip = (ULONG)child_entry;
+			context.Rip = (uint64)ChildEntry;
 #else
-			context.Eip = (ULONG)child_entry;
+			context.Eip = (uint32)ChildEntry;
 #endif
 
 #if _WIN64
-			::ZwQueryVirtualMemory(NtCurrentProcess(), (PVOID)context.Rsp,
+			ZwQueryVirtualMemory(NtCurrentProcess(), (PVOID)context.Rsp,
 								 MemoryBasicInformation, &mbi, sizeof mbi, 0);
 #else
-			::ZwQueryVirtualMemory(NtCurrentProcess(), (PVOID)context.Esp,
+			ZwQueryVirtualMemory(NtCurrentProcess(), (PVOID)context.Esp,
 								 MemoryBasicInformation, &mbi, sizeof mbi, 0);
 #endif
 
@@ -115,28 +279,28 @@ namespace Process
 			stack.ExpandableStackBottom = mbi.AllocationBase;
 
 			// create thread using the modified context and stack
-			if (FAILED(::ZwCreateThread(&hThread, THREAD_ALL_ACCESS, &oa, hProcess,
-				&cid, &context, &stack, TRUE))
+			if (FAILED(ZwCreateThread(&hThread, THREAD_ALL_ACCESS, &oa, hProcess,
+				&cid, &context, &stack, TRUE)))
 				return -1;
 
 			// copy exception table
-			::ZwQueryInformationThread(NtCurrentThread(), ThreadBasicInformation,
+			ZwQueryInformationThread(NtCurrentThread(), ThreadBasicInformation,
 									 &tbi, sizeof tbi, 0);
 			tib = (PNT_TIB)tbi.TebBaseAddress;
-			::ZwQueryInformationThread(hThread, ThreadBasicInformation,
+			ZwQueryInformationThread(hThread, ThreadBasicInformation,
 				&tbi, sizeof tbi, 0);
-			::ZwWriteVirtualMemory(hProcess, tbi.TebBaseAddress,
+			ZwWriteVirtualMemory(hProcess, tbi.TebBaseAddress,
 				&tib->ExceptionList, sizeof tib->ExceptionList, 0);
 
 			// start (resume really) the child
-			::ZwResumeThread(hThread, 0);
+			ZwResumeThread(hThread, 0);
 
 			// clean up
-			::ZwClose(hThread);
-			::ZwClose(hProcess);
+			ZwClose(hThread);
+			ZwClose(hProcess);
 
 			// exit with child's pid
-			return (int)cid.UniqueProcess;
+			return (intptr_t)cid.UniqueProcess;
 		}
 
 
