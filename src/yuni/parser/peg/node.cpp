ddbf78e156c2948c7eb267360363136e0c98b66b
Damien GERARD <damien@iwi.me>
2016-01-13 21:50:15 +0900

    parser: using anonymous namespace

--- src/yuni/parser/peg/node.cpp
+++ src/yuni/parser/peg/node.cpp
@@ -48,186 +48,176 @@ namespace Parser
 namespace PEG
 {
 
-	/* static inline AnyString RuleTypeToString(Node::Type rule)
+	namespace // anonymous
 	{
-		switch (rule)
+
+		template<class StreamT>
+		static inline void PrintText(StreamT& out, const String& text)
 		{
-			case Node::asRule:   return "rule";
-			case Node::asString: return "string";
-			case Node::asSet:    return "set";
-			case Node::asAND:    return "and";
-			case Node::asOR:     return "or";
-		}
-		return "";
-	}*/
+			String::const_utf8iterator end = text.utf8end();
+			String::const_utf8iterator i   = text.utf8begin();
 
-
-	template<class StreamT>
-	static inline void PrintText(StreamT& out, const String& text)
-	{
-		String::const_utf8iterator end = text.utf8end();
-		String::const_utf8iterator i   = text.utf8begin();
-
-		for (; i != end; ++i)
-		{
-			if (i->size() == 1)
+			for (; i != end; ++i)
 			{
-				switch ((char) *i)
+				if (i->size() == 1)
 				{
-					case '\n': out << "\\\\n"; break;
-					case '\t': out << "\\\\t"; break;
-					case '\r': out << "\\\\r"; break;
-					case '\f': out << "\\\\f"; break;
-					case '\v': out << "\\\\v"; break;
-					case '"' : out << "\\\\\""; break;
-					default: out << *i;
+					switch ((char) *i)
+					{
+						case '\n': out << "\\\\n"; break;
+						case '\t': out << "\\\\t"; break;
+						case '\r': out << "\\\\r"; break;
+						case '\f': out << "\\\\f"; break;
+						case '\v': out << "\\\\v"; break;
+						case '"' : out << "\\\\\""; break;
+						default: out << *i;
+					}
 				}
-			}
-			else
-				out << *i;
-		}
-	}
-
-
-
-	static inline void PrintSubNodesIDs(Clob& out, const Node& node)
-	{
-		if (node.rule.type == Node::asRule and node.rule.text == "EOF")
-			return;
-
-		out << "\t\t\"" << node.id << "\" [label = \"";
-		if (node.match.negate)
-			out << "! ";
-
-		switch (node.rule.type)
-		{
-			case Node::asAND:
-			{
-				out << "AND";
-				break;
-			}
-			case Node::asOR:
-			{
-				out << "OR";
-				break;
-			}
-			case Node::asString:
-			{
-				out << "\\\"";
-				PrintText(out, node.rule.text);
-				out << "\\\"";
-				break;
-			}
-			case Node::asSet:
-			{
-				out << "one of \\\"";
-				PrintText(out, node.rule.text);
-				out << "\\\"";
-				break;
-			}
-			case Node::asRule:
-			{
-				PrintText(out, node.rule.text);
-				break;
-			}
-			default:
-				PrintText(out, node.rule.text);
-		}
-
-		out << "\"";
-
-		if (node.rule.type == Node::asAND or node.rule.type == Node::asOR)
-			out << " shape = diamond";
-
-		out << "];\n";
-		for (uint i = 0; i != (uint) node.children.size(); ++i)
-			PrintSubNodesIDs(out, node.children[i]);
-	}
-
-
-	static inline void PrintSubNodesLinks(Clob& out, const Node::Map& rules, const Node& node, const String& source, bool inverseColor)
-	{
-		if (node.rule.type == Node::asRule)
-			return;
-
-		if (node.rule.type == Node::asRule)
-		{
-			Node::Map::const_iterator i = rules.find(node.rule.text);
-			if (i != rules.end())
-				out << "\t\t\"" << source << "\" -> \"" << i->second.id << "\"";
-			else
-				out << "\t\t\"" << source << "\" -> \"" << node.id << "\"";
-		}
-		else
-			out << "\t\t\"" << source << "\" -> \"" << node.id << "\"";
-
-		out << ";\n";
-
-		// relation
-		if (not (node.match.min == 1 and node.match.max == 1))
-		{
-			out << "\t\t\"" << node.id << "\" -> \"" << node.id << "\" [";
-			if (node.rule.type == Node::asRule)
-				out << "lhead = \"cluster-" << node.rule.text << "\"; ";
-			out << "label=<<font color=\"#FF5500\">";
-
-			if (node.match.min == 0 and node.match.max == (uint) -1)
-				out << '*' << node.id;
-			else if (node.match.min == 0 and node.match.max == 1)
-				out << '?';
-			else if (node.match.min == 1 and node.match.max == (uint) -1)
-				out << '+';
-			else
-			{
-				out << '{' << node.match.min << ',';
-				if (node.match.max == (uint) -1)
-					out << 'n';
 				else
-					out << node.match.max;
-				out << '}';
+					out << *i;
 			}
-
-			out << "</font>>];\n";
 		}
 
-		// display all subnodes
-		if (not node.children.empty())
+
+		static inline void PrintSubNodesIDs(Clob& out, const Node& node)
 		{
-			out << "\t\tsubgraph \"cluster-" << node.id << "\" {\n";
-			if (inverseColor)
-			{
-				out << "\t\tstyle = filled;\n";
-				out << "\t\tcolor = lightgrey;\n";
-				out << "\t\tnode [style = filled, color = white];\n";
-			}
-			else
-			{
-				out << "\t\tstyle = filled;\n";
-				out << "\t\tcolor = \"#dfdfdf\";\n";
-				out << "\t\tnode [style = filled, color = lightgrey];\n";
-			}
+			if (node.rule.type == Node::asRule and node.rule.text == "EOF")
+				return;
+
+			out << "\t\t\"" << node.id << "\" [label = \"";
+			if (node.match.negate)
+				out << "! ";
 
 			switch (node.rule.type)
 			{
+				case Node::asAND:
+				{
+					out << "AND";
+					break;
+				}
 				case Node::asOR:
 				{
-					PrintSubNodesLinks(out, rules, node.children[0], node.id, not inverseColor);
-					for (uint i = 1; i < (uint) node.children.size(); ++i)
-						PrintSubNodesLinks(out, rules, node.children[i], node.id, not inverseColor);
+					out << "OR";
+					break;
+				}
+				case Node::asString:
+				{
+					out << "\\\"";
+					PrintText(out, node.rule.text);
+					out << "\\\"";
+					break;
+				}
+				case Node::asSet:
+				{
+					out << "one of \\\"";
+					PrintText(out, node.rule.text);
+					out << "\\\"";
+					break;
+				}
+				case Node::asRule:
+				{
+					PrintText(out, node.rule.text);
 					break;
 				}
 				default:
-				{
-					PrintSubNodesLinks(out, rules, node.children[0], node.id, not inverseColor);
-					for (uint i = 1; i < (uint) node.children.size(); ++i)
-						PrintSubNodesLinks(out, rules, node.children[i], node.children[i - 1].id, not inverseColor);
-				}
+					PrintText(out, node.rule.text);
 			}
 
-			out << "\t\t}\n";
-		}
-	}
+			out << "\"";
 
+			if (node.rule.type == Node::asAND or node.rule.type == Node::asOR)
+				out << " shape = diamond";
+
+			out << "];\n";
+			for (uint i = 0; i != (uint) node.children.size(); ++i)
+				PrintSubNodesIDs(out, node.children[i]);
+		}
+
+
+		static inline void PrintSubNodesLinks(Clob& out, const Node::Map& rules, const Node& node, const String& source, bool inverseColor)
+		{
+			if (node.rule.type == Node::asRule)
+				return;
+
+			if (node.rule.type == Node::asRule)
+			{
+				Node::Map::const_iterator i = rules.find(node.rule.text);
+				if (i != rules.end())
+					out << "\t\t\"" << source << "\" -> \"" << i->second.id << "\"";
+				else
+					out << "\t\t\"" << source << "\" -> \"" << node.id << "\"";
+			}
+			else
+				out << "\t\t\"" << source << "\" -> \"" << node.id << "\"";
+
+			out << ";\n";
+
+			// relation
+			if (not (node.match.min == 1 and node.match.max == 1))
+			{
+				out << "\t\t\"" << node.id << "\" -> \"" << node.id << "\" [";
+				if (node.rule.type == Node::asRule)
+					out << "lhead = \"cluster-" << node.rule.text << "\"; ";
+				out << "label=<<font color=\"#FF5500\">";
+
+				if (node.match.min == 0 and node.match.max == (uint) -1)
+					out << '*' << node.id;
+				else if (node.match.min == 0 and node.match.max == 1)
+					out << '?';
+				else if (node.match.min == 1 and node.match.max == (uint) -1)
+					out << '+';
+				else
+				{
+					out << '{' << node.match.min << ',';
+					if (node.match.max == (uint) -1)
+						out << 'n';
+					else
+						out << node.match.max;
+					out << '}';
+				}
+
+				out << "</font>>];\n";
+			}
+
+			// display all subnodes
+			if (not node.children.empty())
+			{
+				out << "\t\tsubgraph \"cluster-" << node.id << "\" {\n";
+				if (inverseColor)
+				{
+					out << "\t\tstyle = filled;\n";
+					out << "\t\tcolor = lightgrey;\n";
+					out << "\t\tnode [style = filled, color = white];\n";
+				}
+				else
+				{
+					out << "\t\tstyle = filled;\n";
+					out << "\t\tcolor = \"#dfdfdf\";\n";
+					out << "\t\tnode [style = filled, color = lightgrey];\n";
+				}
+
+				switch (node.rule.type)
+				{
+					case Node::asOR:
+					{
+						PrintSubNodesLinks(out, rules, node.children[0], node.id, not inverseColor);
+						for (uint i = 1; i < (uint) node.children.size(); ++i)
+							PrintSubNodesLinks(out, rules, node.children[i], node.id, not inverseColor);
+						break;
+					}
+					default:
+					{
+						PrintSubNodesLinks(out, rules, node.children[0], node.id, not inverseColor);
+						for (uint i = 1; i < (uint) node.children.size(); ++i)
+							PrintSubNodesLinks(out, rules, node.children[i], node.children[i - 1].id, not inverseColor);
+					}
+				}
+
+				out << "\t\t}\n";
+			}
+		}
+
+
+	} // anonymous namepace
 
 
 
