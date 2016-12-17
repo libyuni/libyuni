236540428f4c0e48ae74e3f38a277f50b87c66c6
Loomchild <loomchild@gmail.com>
2016-12-17 12:33:50 +0100

    Fixed OpenAL flags in yuni-config using devpacks.

--- src/yuni/cmake/deps/openal-devpack.cmake
+++ src/yuni/cmake/deps/openal-devpack.cmake
@@ -5,4 +5,7 @@ DEVPACK_IMPORT_OPENAL()
 # We get OPENAL_INCLUDE_DIR from here.
 # We get OPENAL_LIBRARY from here.
 
+LIBYUNI_CONFIG_INCLUDE_PATH("both" "media" "${OPENAL_INCLUDE_DIR}")
+LIBYUNI_CONFIG_LIB_RAW_COMMAND("both" "media" "${OPENAL_LIBRARY}")
+include_directories(${OPENAL_INCLUDE_DIR})
 
